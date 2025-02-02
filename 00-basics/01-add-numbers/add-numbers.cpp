0a508c7a86cc090968a048b0a63cc2a144e599a3
nirglus <nirgluskin@gmail.com>
2025-02-02 11:12:44 +0200

    init commit

---
+++ 00-basics/01-add-numbers/add-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+void main(){
+    int num1, num2;
+    cout << "Enter 2 numbers:";
+    cin >> num1;
+    cin >> num2;
+    cout << "Number 1: " << num1 << ", Number 2: " << num2;
+}

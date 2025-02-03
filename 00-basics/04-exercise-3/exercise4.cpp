49f2d20910f4ac5bd627ef308430fc6eef1a9bfb
nirglus <nirgluskin@gmail.com>
2025-02-03 13:41:30 +0200

    complete ex. 3

---
+++ 00-basics/04-exercise-3/exercise4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+void main(){
+    char start = 'A';
+    char end = 'Z';
+    int count = end - start + 1;
+    cout << "There are " << count << " letters in English.";
+}

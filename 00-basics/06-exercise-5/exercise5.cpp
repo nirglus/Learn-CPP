290bf731477af7bbf43724c583359951408ad532
nirglus <nirgluskin@gmail.com>
2025-02-05 13:24:25 +0200

    complete task 5

--- 00-basics/06-exercise-5/exercise5.cpp
+++ 00-basics/06-exercise-5/exercise5.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+const int floorDelay = 5;
+const int floorToFloorTime = 3;
+
 void main(){
-    
+    int currentFloor, destinationFloor, totalTime;
+    cout << "Enter your current floor number: ";
+    cin >> currentFloor;
+    cout << "Enter your destination floor number: ";
+    cin >> destinationFloor;
+    totalTime = (destinationFloor - currentFloor) * floorToFloorTime;
+    cout << "\nIt will take " << totalTime << " seconds for the elevator to reach the " << destinationFloor << " floor";
 }

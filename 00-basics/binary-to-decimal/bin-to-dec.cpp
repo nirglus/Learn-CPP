fc557c6a96cf557522625443cbc219c00141637d
nirglus <nirgluskin@gmail.com>
2025-03-12 13:35:00 +0200

    complete task

---
+++ 00-basics/binary-to-decimal/bin-to-dec.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+//01000110
+void main(){
+    int res = 0;
+    res = res * 2 + 0;
+    res = res * 2 + 1;
+    res = res * 2 + 0;
+    res = res * 2 + 0;
+    res = res * 2 + 0;
+    res = res * 2 + 1;
+    res = res * 2 + 1;
+    res = res * 2 + 0;
+    cout << "The binary number of 01000110 is " << res << " in decimal\n";
+}
